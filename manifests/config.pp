5fd061af91b2526ab1b4b67be372a861d9f5f552
Philipp Dorschner <philipp.dorschner@netways.de>
2020-01-10 09:30:58 +0100

    linting, typos

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,12 +1,11 @@
 # @summary A short summary of the purpose of this class
 #
-# A description of what this class does
+# Handles the config files for go-carbon
 #
 # @example
 #   include go_carbon::config
 
 class go_carbon::config {
-  $user     = $go_carbon::user
   $conf_dir = $go_carbon::conf_dir
 
   file { "${conf_dir}/go-carbon.conf":
@@ -19,8 +18,8 @@ class go_carbon::config {
     content => template('go_carbon/storage-aggregation.conf.erb')
   }
 
-  #file { "${conf_dir}/storage-schemas.conf":
-  #  ensure  => file,
-  #  content => template('go_carbon/storage-schemas.conf.erb')
-  #}
+  file { "${conf_dir}/storage-schemas.conf":
+    ensure  => file,
+    content => template('go_carbon/storage-schemas.conf.erb')
+  }
 }
--- spec/classes/go_carbon_spec.rb
+++ spec/classes/go_carbon_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'go_carbon::globals' do
+describe 'go_carbon' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
